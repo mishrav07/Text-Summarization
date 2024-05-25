42c7e6364bb2b8429e61de6ff278e234c7862a2e
mishrav07 <vikasmishra218@gmail.com>
2024-05-25 23:09:02 +0530

    Updated Data Ingestion

--- src/textSummarizer/logging/__init__.py
+++ src/textSummarizer/logging/__init__.py
@@ -15,7 +15,7 @@ logging.basicConfig(
 
     handlers = [
         logging.FileHandler(log_filepath),
-        logging.StreamHandler(log_filepath)
+        logging.StreamHandler(sys.stdout)
     ]
 )
 
